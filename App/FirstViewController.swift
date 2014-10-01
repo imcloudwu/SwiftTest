90e29db6c470cae2d4d9de7c7dce57b14aa5eacb
Cloud <imcloudwu@gmail.com>
2014-10-01 12:22:09 +0800

    Initial Commit

---
+++ App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  App
+//
+//  Created by Cloud on 10/1/14.
+//  Copyright (c) 2014 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
